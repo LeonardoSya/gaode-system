69fc3f5669f970d34b5f4236e180ed49627982dd
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-02-27 20:54:51 +0800

    new file:   package-lock.json 	modified:   package.json 	deleted:    src/assets/react.svg 	new file:   src/assets/svg/google-maps-icon.svg 	new file:   src/components/map-icon.css 	modified:   src/maps/map-container.tsx 	modified:   vite.config.ts 	new file:   yarn.lock

--- src/maps/map-container.tsx
+++ src/maps/map-container.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './map-container.module.css';
 import AMapLoader from '@amap/amap-jsapi-loader';
+import "../components/map-icon.css"
 
-const MapContainer = () => {
+const MapContainer: React.FC = () => {
+	const mapRef = useRef(null);
 
 	useEffect(() => {
 
@@ -30,16 +32,19 @@ const MapContainer = () => {
 					zIndex: 10,
 					heightFactor: 2,
 				});
-
+				// Initialize map
 				const map = new AMap.Map("container", {
 					viewMode: "3D",
 					zoom: 17,
-					center: [116.4, 39.9],
+					center: [116.345983, 40.003175],
 					pitch: 30,
 					rotation: -15,
 					showBuildingBlock: false,
 					layers: [satellite, roadNet, buildings],
+					mapStyle: 'amap://styles/light',
+					immediately: false,
 				});
+				mapRef.current = map;
 
 				// 异步加载插件
 				AMap.plugin([
@@ -49,33 +54,49 @@ const MapContainer = () => {
 					"AMap.AutoComplete",
 					'AMap.Driving',
 					'AMap.Polyline',
-				], function () {
-					// 3D罗盘
-					// const controlBar = new AMap.ControlBar({
-					// 	position: { top: "2vh", right: "1vw" }
-					// });
-					// 输入提示插件
-					// const autoOptions = { city: "全国", input: "map-search", };
-					// const autoComplete = new AMap.Autocomplete(autoOptions);
-					// 定位
+				], () => {
 
-					// map.addControl(controlBar);
+					map.on('complete', function completeFC() {
+
+						// 3D罗盘
+						const controlBar = new AMap.ControlBar({
+							position: { top: "2vh", right: "1vw" }
+						});
+						map.addControl(controlBar);
+
+						// Marker
+						const marker = new AMap.Marker({
+							position: new AMap.LngLat(116.345983, 40.003175),
+							title: '林大',
+							content: '' +'<div class="loader-shape-3"></div>',
+							// offset: new AMap.Pixel(-13, -30)/,
+							zooms:[15,20],
+							anchor:"bottom-center",
+						});
+						map.add(marker);
+						const clickHandlerPosition = (e: { lnglat: { getLng: () => number; getLat: () => number; }; }) => {
+							console.log([e.lnglat.getLng(), e.lnglat.getLat()])
+						};
+						
+						map.on('click', clickHandlerPosition);
+						map.setFitView();
+					})
 
 				})
 
-				map.setMapStyle('amap://styles/light')
 				map.add(traffic);
 
-				return () => map?.destroy();
-
+				return () => {
+					map?.destroy();
+				};
 			} catch (error) {
-				console.log(error)
+				console.error(error)
 			}
 		};
 
 		loadMap();
 
-	}, []);
+	}, [mapRef]);
 
 	return (
 		<div
